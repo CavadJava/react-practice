eeeb831c7596f88be557db9fabf9d2e3cdf43b63
javad <j.bagirov@expressbank.az>
2025-11-04 18:26:32 +0400

    Switch to App component and add basic structure
    Replaced the Car component with the newly created App component as the default render target. Introduced a basic App.jsx file with a "Hello World!" heading to serve as the main application entry point.

--- exercise001/src/main.jsx
+++ exercise001/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import Car from "./Car.jsx";
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <Car year={1969} />
-);
+    <App />
+)
