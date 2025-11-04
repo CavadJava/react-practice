85004931d915bbad024adb39030a39e2beae53a7
javad <j.bagirov@expressbank.az>
2025-11-04 18:18:02 +0400

    Remove unused Profile component

--- exercise02/src/components/Profile.jsx
+++
@@ -1,8 +0,0 @@
-import Button from "./button/index.jsx";
-
-function Profile() {
-    <Button alertEnable={false}>No Click</Button>
-    <Button label={"Click button"} alertEnable={true} alertText={"Hello React"}>Click Button</Button>
-}
-
-export default Profile
