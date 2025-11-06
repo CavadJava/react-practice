7dd034cb4546190a9e8dd61c8dfb3f9093312543
javad <j.bagirov@expressbank.az>
2025-11-06 20:01:50 +0400

    Add Button component with styles and prop validation
    - Created `Button.jsx` with configurable props: `size`, `variant`, `disabled`, `showArrow`, and `children`.
    - Defined CSS styles in `Button.module.css` with support for various sizes and variants.
    - Added PropTypes for robust prop validation.

---
+++ exercise-strong-props-01/src/components/Button.jsx
@@ -0,0 +1,30 @@
+import styles from "./Button.module.css";
+import PropTypes from 'prop-types';
+
+export const Button = ({size = "medium", variant = "primary", disabled = false, showArrow = false, children}) => {
+// export const Button = ({size, variant, disabled, showArrow, children}) => {
+
+    const sizeClassName = styles[size] || '';
+    const variantClassName = styles[variant] || '';
+    const arrowNext = showArrow ? ' -> ' : '';
+
+    return (
+        <button className={`${styles.btn} ${sizeClassName} ${variantClassName}`} disabled={disabled}>
+            {children} {arrowNext}
+        </button>
+    )
+}
+Button.propTypes = {
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    variant: PropTypes.oneOf(['primary', 'secondary', 'success', 'warning', 'info', 'light', 'dark']),
+    disabled: PropTypes.bool,
+    showArrow: PropTypes.bool,
+    children: PropTypes.node.isRequired
+}
+
+// Button.defaultProps = {
+//     size: 'medium',
+//     variant: 'primary',
+//     disabled: false,
+//     showArrow: false
+// }
