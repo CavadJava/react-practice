1871aa299ff131e43058860135b6e01443a44e89
javad <j.bagirov@expressbank.az>
2025-11-10 01:23:04 +0400

    Add Task component with Photo, Info, and SkillSet subcomponents
    - Implemented `Task.jsx` containing `Photo`, `Info`, and `SkillSet` subcomponents to display user details and skills.
    - Styled elements with inline styles and dynamic props.

---
+++ exercise-task-react-bootstrap/src/components/task02/Task.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+function Task() {
+    return (
+        <div className="container">
+            <Photo/>
+            <Info/>
+            <SkillSet name="React" emoji=":react:" color="red"/>
+        </div>
+    )
+}
+
+function Photo(){
+    return (
+        <div className="container">
+            <img style={{width: '200px',height: '300px'}}
+                src="https://image.tmdb.org/t/p/w500/whNwkEQYWLFJA8ij0WyOOAD5xhQ.jpg" alt="Random" />
+        </div>
+    );
+}
+function Info(){
+    return (
+      <div className="container">
+          <h1 style={{width:'500px',fontSize:'22px'}}>I am Javad. I am Software Engineer from Baku, Azerbaijan</h1>
+      </div>
+    );
+}
+function SkillSet(){
+    return (
+        <div className="container d-flex flex-wrap gap-2">
+            <Skill name="React" emoji="💪" color="green"/>
+            <Skill name="JavaScript" emoji="💪" color="orangered"/>
+            <Skill name="HTML+CSS" emoji="💪" color="yellow"/>
+        </div>
+    );
+}
+function Skill({name, emoji, color}){
+    return(<div className="skill" style={{backgroundColor: color}}>
+        <span>{name}</span>
+        <span>{emoji}</span>
+    </div>
+    );
+}
+
+export default Task;
