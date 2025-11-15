97781f4e7ba5a67876c313510cd92b5dcba81b18
javad <j.bagirov@expressbank.az>
2025-11-15 05:03:16 -0800

    added HideItemsOnClick task

--- exercise-task-react-bootstrap/src/components/props/Task.jsx
+++ exercise-task-react-bootstrap/src/components/props/Task.jsx
@@ -5,9 +5,10 @@ import ShowTextTask from "./ShowTextTask";
 import VideoPlayerTask from "./VideoPlayerTask";
 import TimeoutTask from "./TimoutTask";
 import HideItemOnClick from "./HideItemOnClick";
+import HideItemsOnClick from "./HideItemsOnClick";
 
 function Task(){
-    return HideItemOnClick();
+    return HideItemsOnClick();
 }
 
 export default Task
