46598a15b183a2c4b6510a4cd24c50fdd505ebc1
javad <j.bagirov@expressbank.az>
2025-11-16 04:42:39 -0800

    added features Double number

--- exercise-task-react-bootstrap/src/components/props/13112025/double/DoubleNumber.jsx
+++ exercise-task-react-bootstrap/src/components/props/13112025/double/DoubleNumber.jsx
@@ -5,13 +5,16 @@ function DoubleNumber() {
 
     const [number, setNumber] = useState(1);
     const inputRef = useRef();
+    const [stopDouble, setStopDouble] = useState(false);
+    const [doubleNumberList, setDoubleNumberList] = useState([]);
+    const [isFullDoubleNumberList, setIsFullDoubleNumberList] = useState(false);
 
     const divStyle = {
         padding: "20px",
         textAlign: "center",
         borderRadius: "16px",
         padding: "24px auto",
-        maxWidth: "200px",
+        maxWidth: "230px",
     }
 
     const pStyle = {
@@ -24,9 +27,27 @@ function DoubleNumber() {
         color: "white",
         border: "none",
     }
+    const resultStyle = {
+        padding: "4px 2px",
+        fontSize: "18px",
+        borderRadius: "20px",
+        cursor: "pointer",
+        backgroundColor: "purple",
+        color: "white",
+        border: "none",
+    }
 
     function handleClick() {
-        setNumber(Number(inputRef.current.textContent) * 2)
+        if(stopDouble){
+            return;
+        }
+
+        if(isFullDoubleNumberList){
+            return
+        }else{
+            setIsFullDoubleNumberList(false);
+            setNumber(Number(inputRef.current.textContent) * 2)
+        }
     }
 
 
@@ -34,6 +55,42 @@ function DoubleNumber() {
         <div className="container" style={divStyle}>
             <p ref={inputRef} style={pStyle}>{number}</p>
             <button onClick={handleClick} className="btn btn-primary">Double Number</button>
+            <br></br>
+
+            <button className="btn btn-primary"  onClick={()=>{
+                if(doubleNumberList.length>=3){
+                    alert("Double Number List is full");
+                    setIsFullDoubleNumberList(true);
+                }else{
+                    setDoubleNumberList([...doubleNumberList, number]);
+                }
+            }}>Add Double Number</button>
+            <p></p>
+
+            <button className="btn btn-danger"  onClick={()=>{
+                setStopDouble(true);
+            }}>Stop</button>
+            <button className="btn btn-warning"  onClick={()=>{
+                setStopDouble(false);
+            }}>Continue</button>
+            <button className="btn btn-success" onClick={()=>{
+                setStopDouble(false);
+                setNumber(1);
+                inputRef.current.textContent = 1;
+                if(isFullDoubleNumberList){
+                    setIsFullDoubleNumberList(false);
+                    setDoubleNumberList([]);
+                }
+
+            }}>Reset</button>
+            <div className="row">
+                <p style={pStyle}>Double Number List</p>
+                {
+                    doubleNumberList.map((item, index) => {
+                        return <p style={resultStyle} key={index}>{item}</p>
+                    })
+                }
+            </div>
         </div>
     );
 }
