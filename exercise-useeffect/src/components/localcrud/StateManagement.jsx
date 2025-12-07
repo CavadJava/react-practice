89a510d657117e6c672db0722ebf2788f4bec05d
frontend <frontend@javads-MacBook-Pro.local>
2025-12-08 02:05:33 +0400

    Updated Statement Management

--- exercise-useeffect/src/components/localcrud/StateManagement.jsx
+++ exercise-useeffect/src/components/localcrud/StateManagement.jsx
@@ -1,4 +1,7 @@
-import { useRef, useState } from "react";
+import { useId, useRef, useState } from "react";
+import PropTypes from "prop-types";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styles from "./MeButton.module.css";
 
 function StateManagement(){
 
@@ -10,44 +13,79 @@ function StateManagement(){
     const ageRef = useRef("")
 
     const addCardHandle = (e)=>{
-        card.fullname = fullNameref.current.value;
-        card.nickname = nickNameref.current.value;
-        card.age = ageRef.current.value;
-        console.log(card)
-        setCards([...cards,card])
+        const newCard = {
+            id:  Date.now() + Math.random(),
+            fullname:  fullNameref.current.value,
+            nickname:  nickNameref.current.value,
+            age: ageRef.current.value
+        };
+        console.log(newCard)
+        setCards([...cards,newCard])
 
     }
 
     return (
         <>
             <div className="container">
-                <input type="text" placeholder="FullName" ref={fullNameref}/><p/>
-                <input type="text" placeholder="NickName" ref={nickNameref}/><p/>
-                <input type="number" ref={ageRef}/><p/>
-                <div className="groupbutton">
-                    <button onClick={(e)=>addCardHandle(e)}>Add to Card</button>
-                    <button>RemoveAll</button>
+                <section>
+                    <input type="text" className="form-data" placeholder="FullName" ref={fullNameref}/><p/>
+                    <input type="text" placeholder="NickName" ref={nickNameref}/><p/>
+                    <input type="number" ref={ageRef}/><p/>
+                    <div className="groupbutton">
+                        <button className="btn btn-primary" onClick={(e)=>addCardHandle(e)}>Add to Card</button>
+                        <button>RemoveAll</button>
+                    </div>
+                </section>
+                <div className="card w-100 d-flex justify-content-between flex-row gap-5 .border-0">
+                    {
+                        cards.map((card)=>{
+                            return (
+                                 <CardItem key={card.id} {...card}/>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </>
     )
 
 }
-function CardItem(){
+function CardItem({id, fullname, nickname, age}){
+
+    const happy = () => {
+        alert("Hello");
+    }
 
     return(
         <>
-            <div className="carditem">
-                <p>FullName: </p>
-                <p>NickName: </p>
-                <p>Age: </p>
+            <div className="card-body" style={{backgroundColor: "gray"}}>
+                <p>FullName: {fullname}</p>
+                <p>NickName: {nickname}</p>
+                <p>Age: {age}</p>
                 <div className="carditem-groupbutton">
                     <button>Edit</button>
-                    <button>Remove</button>
+                    <MeButton _name="Remove" _colorType="yellow" happy={happy} />
                 </div>
             </div>
         </>
     )
+}
+
+export const MeButton = ({_name, _colorType, defaultColor="btn",happy})=>{
+
+
+    const colorClassName = styles[_colorType] || '';
+    const defaultColorValue = defaultColor;
+    
+
+    return (
+        <button className={`${defaultColorValue} ${colorClassName}`} onClick={()=>{happy()}}>{_name}</button>
+    )
+}
+MeButton.prototype = {
+    name : PropTypes.string.isRequired,
+    colorType: PropTypes.oneOf(['blue', 'red', 'yellow']),
+    defaultColor: PropTypes.string
 }
 
 
