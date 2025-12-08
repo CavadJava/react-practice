eeae99d60de095aab7168299d3b6a5c2cdb55c60
cavadjava <cavadjava@gmail.com>
2025-12-08 04:28:50 +0400

    feat: enhance LoginForm with local storage integration and data display

--- exercise-formik/src/components/LoginForm.jsx
+++ exercise-formik/src/components/LoginForm.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFormik } from 'formik';
 
 const LoginForm = () => {
+    const [data, setData] = React.useState([]);
     const formik = useFormik({
         initialValues: {
             email: '',
@@ -9,32 +10,57 @@ const LoginForm = () => {
         },
         onSubmit: values => {
             console.log('Form data', values);
+            setData([...data, values]);
+            window.localStorage.setItem('userData', JSON.stringify([...data, values]));
             alert(JSON.stringify(values, null, 2));
         }
     });
+    useEffect(() =>{
+        console.log("Component mounted");
+        const storeData = JSON.parse(window.localStorage.getItem('userData'));
+        if(storeData){
+            setData(storeData);
+        }
+    },[])
+
+    const handleRemoveAll = () => {
+        window.localStorage.removeItem('userData');
+        setData([]);
+    }
     
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="email">Email Address</label>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-            />
-            
-            <label htmlFor="password">Password</label>
-            <input
-                id="password"
-                name="password"
-                type="password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-            />
-            
-            <button type="submit">Submit</button>
-        </form>
+        <div className='container'>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="email">Email Address</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+                
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                />
+                
+                <button type="submit">Submit</button>
+                <button type="button" onClick={handleRemoveAll}>Remove All</button>
+            </form>
+            <div className='cards'>
+                {data.map((item, index) => (
+                    <div className='card' key={index}>
+                        <p><strong>Email:</strong> {item.email}</p>
+                        <p><strong>Password:</strong> {item.password}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     );
 }
 export default LoginForm;
