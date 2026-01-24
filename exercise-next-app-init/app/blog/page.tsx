03d8a3904b53706f7c91a0e053ea0d3d448ab4bc
cavadjava <cavadjava@gmail.com>
2026-01-25 02:09:45 +0400

    refactor: update About component to use plain text instead of heading

--- exercise-next-app-init/app/blog/page.tsx
+++ exercise-next-app-init/app/blog/page.tsx
@@ -3,7 +3,7 @@ import Countdown from "@/components/Countdown";
 export default function About() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>About</h1>
+      About Page
       <Countdown from={10} to={0} />
     </div>
   );
