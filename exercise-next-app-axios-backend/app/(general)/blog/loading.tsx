071cd408bb687a809680e84a9619d4c12a98b0a4
cavadjava <cavadjava@gmail.com>
2026-04-27 08:48:28 +0400

    removed deleted exercies-next-app commit

--- exercise-next-app-axios-backend/app/(general)/blog/loading.tsx
+++
@@ -1,6 +0,0 @@
-// LoadingSkeleton yoxdur
-export default function Loading() {
-    return <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-        <h1>Loading Blog Page...</h1>
-    </div>;
-}
